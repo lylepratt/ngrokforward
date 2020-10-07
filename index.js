add7af175d5e7a34f74d254fa4cd2b967e46ae55
Lyle Pratt <lylepratt@gmail.com>
2020-10-07 09:36:21 -0500

    Pushing

---
+++ index.js
@@ -0,0 +1,10 @@
+var httpProxy = require('http-proxy');
+var url       = require('url');
+const proxy = httpProxy.createProxyServer({})
+require('http').createServer(function(req, res) {
+    var subdomain = req.headers.host.split(".")[0];
+    var target = `https://${subdomain}.ngrok.io`
+    proxy.web(req, res, { target: target })
+}).listen(9000, function () {
+    console.log("Waiting for requests...");
+});
