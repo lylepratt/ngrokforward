c2f0097cead11d7370be7cb70c7c8648dabb6027
Lyle Pratt <lylepratt@gmail.com>
2020-10-07 09:58:09 -0500

    using paths

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ var httpProxy = require('http-proxy');
 var url       = require('url');
 const proxy = httpProxy.createProxyServer({})
 require('http').createServer(function(req, res) {
+    console.log(req.url)
     var subdomain = req.headers.host.split(".")[0];
+
+    var routes = req.url.split("/");
+    var subdomain = routes[1];
     var target = `https://${subdomain}.ngrok.io`
+    if(subdomain == "") {
+        target = "https://ngrok.com"
+    }    
     console.log(`Routing to: ${target}`);
     proxy.web(req, res, { target: target })
 }).listen(8080, function () {
