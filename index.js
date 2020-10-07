06bbd5f4571d8d4888d52a146763ebbd6d4d02f6
Lyle Pratt <lylepratt@gmail.com>
2020-10-07 12:07:18 -0500

    fixing

--- index.js
+++ index.js
@@ -17,12 +17,20 @@ app.use('/', createProxyMiddleware({
         var routes = req.url.split("/");
         var subdomain = routes[1];        
         var path = req.url.replace(`/${subdomain}`, "");
-        var target = `http://${subdomain}.ngrok.io${path}`
+        var target = `http://${subdomain}.ngrok.io`
         if(subdomain == "") {
             target = `http://ngrok.com`
         }
-        console.log(target);
+        console.log(`Target: ${target}`);
         return target
+    },
+    pathRewrite: function (path, req) {
+        var subdomain = req.headers.host.split(".")[0];
+        var routes = req.url.split("/");
+        var subdomain = routes[1];        
+        var path = req.url.replace(`/${subdomain}`, "");
+        console.log(`Path: ${path}`);
+        return path;
     }
 }));
 app.listen(8080, function () {
