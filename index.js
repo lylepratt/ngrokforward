dc5a0187477cfb61a607decfa7c7be286b0106d4
Lyle Pratt <lylepratt@gmail.com>
2020-10-07 09:37:50 -0500

    updating port

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ require('http').createServer(function(req, res) {
     var subdomain = req.headers.host.split(".")[0];
     var target = `https://${subdomain}.ngrok.io`
     proxy.web(req, res, { target: target })
-}).listen(9000, function () {
+}).listen(80, function () {
     console.log("Waiting for requests...");
 });
