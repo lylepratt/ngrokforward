e2097c3b99604b2ab1b348e4c12f66355f87da31
Lyle Pratt <lylepratt@gmail.com>
2020-10-07 11:59:08 -0500

    taking another path

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-var httpProxy = require('http-proxy');
-var url       = require('url');
-const proxy = httpProxy.createProxyServer({})
-require('http').createServer(function(req, res) {
-    console.log(req.url)
-    var subdomain = req.headers.host.split(".")[0];
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+ 
+const app = express();
 
-    var routes = req.url.split("/");
-    var subdomain = routes[1];
-    var target = `http://${subdomain}.ngrok.io`
-    if(subdomain == "") {
-        target = "http://ngrok.com"
-    }    
-    console.log(`Routing to: ${target}`);
-    proxy.web(req, res, { target: target })
-}).listen(8080, function () {
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+    res.send('This is a proxy service which proxies to Ngrok Subdomains.');
+});
+
+// Proxy endpoints
+app.use('/', createProxyMiddleware({
+    target: "ngrok.io",
+    changeOrigin: true,
+    router: function(req) {
+        var subdomain = req.headers.host.split(".")[0];
+        var routes = req.url.split("/");
+        var subdomain = routes[1];        
+        var path = req.url.replace(`/${subdomain}`, "");
+        var target = `http://${subdomain}.ngrok.io${path}`
+        if(subdomain == "") {
+            target = `http://ngrok.com`
+        }
+        console.log(target);
+        return target
+    }
+}));
+app.listen(8080, function () {
     console.log("Waiting for requests...");
 });
