fb8be67121235751f8b0371844672f97fe8a3f4c
Lyle Pratt <lylepratt@gmail.com>
2020-10-07 09:44:29 -0500

    adding logging

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const proxy = httpProxy.createProxyServer({})
 require('http').createServer(function(req, res) {
     var subdomain = req.headers.host.split(".")[0];
     var target = `https://${subdomain}.ngrok.io`
+    console.log(`Routing to: ${target}`);
     proxy.web(req, res, { target: target })
 }).listen(80, function () {
     console.log("Waiting for requests...");
